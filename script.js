f5b92fc086e32bd93e73f2095b22b6677382b72c
mmuzammil61 <brickclay61@gmail.com>
2024-09-18 13:46:57 +0500

    copy text update after copied and function update and short

--- script.js
+++ script.js
@@ -5,27 +5,6 @@ const loader = document.getElementById('loader');
 // const url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
 const url = 'https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
 
-// let getJoke = () => {
-//     apiResponse.classList.remove("fade");
-//     loader.style.display = "block";
-//     fetch(url)
-//         .then(data => data.json())
-//         .then(item => {
-//             function greet() {
-//                 loader.style.display = "none";
-//             }
-//             setTimeout(greet, 1000);
-
-//             function greet2() {
-//                 apiResponse.textContent = `${item.joke}`;
-//                 apiResponse.classList.add('fade');
-//             }
-//             setTimeout(greet2, 1000);
-
-//         });
-//     copyJoke.innerText = 'Copy';
-// }
-
 
 const getJoke = () => {
     apiResponse.classList.remove("fade");
