20dce0c163745becb5b02f047ebc040194be8fb6
mmuzammil61 <brickclay61@gmail.com>
2024-09-18 13:45:44 +0500

    copy text update after copied and function update and short

--- script.js
+++ script.js
@@ -5,26 +5,45 @@ const loader = document.getElementById('loader');
 // const url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
 const url = 'https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
 
-let getJoke = () => {
+// let getJoke = () => {
+//     apiResponse.classList.remove("fade");
+//     loader.style.display = "block";
+//     fetch(url)
+//         .then(data => data.json())
+//         .then(item => {
+//             function greet() {
+//                 loader.style.display = "none";
+//             }
+//             setTimeout(greet, 1000);
+
+//             function greet2() {
+//                 apiResponse.textContent = `${item.joke}`;
+//                 apiResponse.classList.add('fade');
+//             }
+//             setTimeout(greet2, 1000);
+
+//         });
+//     copyJoke.innerText = 'Copy';
+// }
+
+
+const getJoke = () => {
     apiResponse.classList.remove("fade");
     loader.style.display = "block";
+
     fetch(url)
         .then(data => data.json())
         .then(item => {
-            function greet() {
+            setTimeout(() => {
                 loader.style.display = "none";
-            }
-            setTimeout(greet, 1000);
-
-            function greet2() {
-                apiResponse.textContent = `${item.joke}`;
-                apiResponse.classList.add('fade');
-            }
-            setTimeout(greet2, 1000);
-
+                apiResponse.textContent = item.joke;
+                apiResponse.classList.add("fade");
+            }, 1000);
         });
-    copyJoke.innerText = 'Copy';
-}
+
+    copyJoke.innerText = "Copy";
+};
+
 
 
 
@@ -35,6 +54,12 @@ copyJoke.addEventListener('click', () => {
         navigator.clipboard.writeText(apiResponse.textContent)
         console.log('Joke copied to clipboard!');
         copyJoke.innerText = 'Copied';
+
+        function textBack (){
+            copyJoke.innerText = 'Copy';
+        }
+
+        setTimeout(textBack, 3000);
     }
 });
 
